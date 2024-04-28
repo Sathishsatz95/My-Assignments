2fb4133f573d89b9f6a92295c10211bbb937d4aa
Sathishsatz95 <168266607+Sathishsatz95@users.noreply.github.com>
2024-04-28 08:27:18 +0530

    Add files via upload
    Day 1

---
+++ day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		int releaseyear = 1998;
+		float browserversion = 100.2f;
+		String browsername = "firefox";
+		boolean isvisible = true;
+		char browserlogo = 'f';
+		
+		System.out.println(releaseyear +"\n" +browserversion +"\n" +browsername +"\n" +isvisible +"\n" +browserlogo);
+	}
+
+}
